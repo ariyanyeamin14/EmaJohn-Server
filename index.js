b2503ba07b406dbfc333c2b324d50410b81276f5
Ariyan Yeamin <yeaminfoysal14@gmail.com>
2024-12-20 09:50:22 -0800

    pagination complete

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ucdi4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,9 +30,19 @@ async function run() {
 
     const productCollection = client.db('emaJohnDB').collection('products');
 
-    app.get('/products', async(req, res) => {
-        const result = await productCollection.find().toArray();
-        res.send(result);
+    app.get('/products', async (req, res) => {
+      const page = parseInt(req.query.page)
+      const size = parseInt(req.query.size)
+      const result = await productCollection.find()
+      .skip(page * size)
+      .limit(size)
+      .toArray();
+      res.send(result);
+    })
+
+    app.get('/productsCount', async (req, res) => {
+      const count = await productCollection.estimatedDocumentCount();
+      res.send({ count })
     })
 
     // Send a ping to confirm a successful connection
@@ -46,10 +56,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/', (req, res) =>{
-    res.send('john is busy shopping')
+app.get('/', (req, res) => {
+  res.send('john is busy shopping')
 })
 
-app.listen(port, () =>{
-    console.log(`ema john server is running on port: ${port}`);
+app.listen(port, () => {
+  console.log(`ema john server is running on port: ${port}`);
 })
